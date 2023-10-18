b12d5911a02bba57e3c6465c44fdf2b1cf590726
Federico Jose Diaz Guanoluiza <fdiaz-gu@c1r12s1.42madrid.com>
2023-10-18 17:07:21 +0200

    added gnl to libft

---
+++ libft/get_next_line.c
@@ -0,0 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fdiaz-gu <fdiaz-gu@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/02 14:37:41 by fdiaz-gu          #+#    #+#             */
+/*   Updated: 2023/10/18 17:05:42 by fdiaz-gu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+char	*ft_write_in_aux(int fd, char *aux)
+{
+	char	*buffer;
+	int		check;
+
+	check = 1;
+	buffer = malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	if (!buffer)
+		return (NULL);
+	while (!(ft_strchr(aux, '\n')) && check != 0)
+	{
+		check = read(fd, buffer, BUFFER_SIZE);
+		if (check == -1)
+		{
+			free(buffer);
+			free(aux);
+			return (NULL);
+		}
+		buffer[check] = '\0';
+		aux = ft_strjoin(aux, buffer);
+	}
+	free(buffer);
+	return (aux);
+}
+
+char	*get_next_line(int fd)
+{
+	char			*line;	
+	static char		*aux;
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	aux = ft_write_in_aux(fd, aux);
+	if (!aux)
+		return (NULL);
+	line = ft_extract_line(aux);
+	aux = ft_new_static(aux);
+	return (line);
+}
---
+++ libft/get_next_line.h
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fdiaz-gu <fdiaz-gu@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/02 14:37:28 by fdiaz-gu          #+#    #+#             */
+/*   Updated: 2023/10/17 16:43:02 by fdiaz-gu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 4
+# endif
+# include <stdlib.h>
+# include <fcntl.h>
+# include <unistd.h>
+# include <stdio.h>
+
+char	*get_next_line(int fd);
+char	*ft_strjoin(char *s1, char *s2);
+char	*ft_extract_line(char *str);
+char	*ft_strchr(const char *s, int c);
+size_t	ft_strlen(const char *str);
+char	*ft_new_static(char *aux);
+
+#endif
