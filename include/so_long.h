5810552708fe953646770da7e2fa2e702b3f614d
Federico Jose Diaz Guanoluiza <fdiaz-gu@c1r9s3.42madrid.com>
2023-10-22 20:17:48 +0200

    modified t_map

--- include/so_long.h
+++ include/so_long.h
@@ -6,7 +6,7 @@
 /*   By: fdiaz-gu <fdiaz-gu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/16 16:42:19 by fdiaz-gu          #+#    #+#             */
-/*   Updated: 2023/10/21 18:22:12 by fdiaz-gu         ###   ########.fr       */
+/*   Updated: 2023/10/22 19:56:17 by fdiaz-gu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,17 +24,21 @@
 # include "../minilibx_opengl/mlx.h"
 # define COLOR_RED "\033[0;31m"
 
+# define PL "./sprites/Down.xpm"
+
 typedef struct s_map
 {
     int     coins;
     int     exit;
-    int     player;
+    int     n_players;
+    int     player_x;
+    int     player_y;
     char    **map;
+    char    **map_copy;
     size_t     map_width;
     size_t     map_height;
-    int     init_pos;
-
-    
+    int     init_pos;    
+            
 }t_map;
 
 void    check_arguments(int argc, char *argv);
