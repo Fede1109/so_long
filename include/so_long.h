153393ebb5bf15fd3f18b3af6ace6637a87896d4
Federico Jose Diaz Guanoluiza <fdiaz-gu@c3r7s2.42madrid.com>
2023-10-28 19:19:53 +0200

    norminette

--- include/so_long.h
+++ include/so_long.h
@@ -6,7 +6,7 @@
 /*   By: fdiaz-gu <fdiaz-gu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/16 16:42:19 by fdiaz-gu          #+#    #+#             */
-/*   Updated: 2023/10/28 19:14:19 by fdiaz-gu         ###   ########.fr       */
+/*   Updated: 2023/10/28 19:19:32 by fdiaz-gu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,52 +38,52 @@
 
 typedef struct s_map
 {
-    int     coins;
-    int     coins_copy;
-    int     coins_copy_2;
-    int     n_players;
-    int     player_copy;
-    int     exit;
-    int     exit_copy;
-    int     exit_copy_2;
-    int     exit_x;
-    int     exit_y;
-    char    **map;
-    char    **map_copy;
-    char    **map_copy_2;
-    size_t     map_width;
-    size_t     map_height;    
-    void    *mlx;
-    void    *mlx_win;
-    int     img_width;
-    int     img_height;
-    int     player_x;
-    int     player_y;
-    int     movements;
-    void    *player_img;
-    void    *coins_img;
-    void    *walls_img;
-    void    *exit_img;
-    void    *floor_img;
+	int			coins;
+	int			coins_copy;
+	int			coins_copy_2;
+	int			n_players;
+	int			player_copy;
+	int			exit;
+	int			exit_copy;
+	int			exit_copy_2;
+	int			exit_x;
+	int			exit_y;
+	char		**map;
+	char		**map_copy;
+	char		**map_copy_2;
+	size_t		map_width;
+	size_t		map_height;
+	void		*mlx;
+	void		*mlx_win;
+	int			img_width;
+	int			img_height;
+	int			player_x;
+	int			player_y;
+	int			movements;
+	void		*player_img;
+	void		*coins_img;
+	void		*walls_img;
+	void		*exit_img;
+	void		*floor_img;
 }t_map;
 
-void    check_arguments(int argc, char *argv);
-void    read_map(char *argv, t_map *map);
-void    init_variables(t_map *map);
-int     check_char_map(t_map *map);
+void	check_arguments(int argc, char *argv);
+void	read_map(char *argv, t_map *map);
+void	init_variables(t_map *map);
+int		check_char_map(t_map *map);
 void	check_rectangle(t_map *map);
 void	count_elements(t_map *map);
 void	check_elements_number(t_map *map);
-int     check_border(t_map *map);
+int		check_border(t_map *map);
 void	assign_exit(int column, int row, t_map *map);
 void	flood_fill_from_player(t_map *map, int column, int row);
 void	flood_fill_from_exit(t_map *map, int column, int row);
 void	check_path(t_map *map);
-int	    end_game();
-int	    win_game(t_map *map);
+int		end_game(void);
+int		win_game(t_map *map);
 int		detect_key(int key_code, t_map *map);
-void    coin_counter(t_map *map);
+void	coin_counter(t_map *map);
 void	draw_map(t_map *map);
-void    ft_error(int n);
+void	ft_error(int n);
 
 #endif
