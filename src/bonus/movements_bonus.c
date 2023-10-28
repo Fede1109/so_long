31943e41b470261048bc2278698b69a86750cde3
Federico Jose Diaz Guanoluiza <fdiaz-gu@c3r7s2.42madrid.com>
2023-10-28 20:34:57 +0200

    print movements

---
+++ src/bonus/movements_bonus.c
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   movements_bonus.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fdiaz-gu <fdiaz-gu@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/28 20:28:11 by fdiaz-gu          #+#    #+#             */
+/*   Updated: 2023/10/28 20:32:48 by fdiaz-gu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../include/so_long.h"
+
+void	movements_on_screen(t_map *map)
+{
+	char	*mov = ft_itoa(map->movements);
+	mlx_string_put(map->mlx, map->mlx_win, 10, 10, 000, mov);
+	free(mov);
+}
