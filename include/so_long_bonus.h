e255fcdbc18e7d258dd4bd0b3efe6122b5d2b308
Federico Jose Diaz Guanoluiza <fdiaz-gu@c3r3s5.42madrid.com>
2023-10-30 16:39:50 +0100

    added new functions

--- include/so_long_bonus.h
+++ include/so_long_bonus.h
@@ -6,7 +6,7 @@
 /*   By: fdiaz-gu <fdiaz-gu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/16 16:42:19 by fdiaz-gu          #+#    #+#             */
-/*   Updated: 2023/10/30 11:11:52 by fdiaz-gu         ###   ########.fr       */
+/*   Updated: 2023/10/30 16:33:15 by fdiaz-gu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,9 +26,6 @@
 # define COLOR_GREEN "\033[0;32m"
 # define COLOR_BLUE "\033[0;36m"
 # define PLAYER "./sprites/down.xpm"
-# define LEFT "./sprites/down.xpm"
-# define RIGHT "./sprites/down.xpm"
-# define UP "./sprites/down.xpm"
 # define COIN "./sprites/coin.xpm"
 # define WALL "./sprites/wall.xpm"
 # define ENEMY "./sprites/enemy.xpm"
@@ -46,6 +43,7 @@ typedef struct s_map
 	int			coins_copy;
 	int			coins_copy_2;
 	int			n_players;
+	int			enemies;
 	int			player_copy;
 	int			exit;
 	int			exit_x;
@@ -67,6 +65,8 @@ typedef struct s_map
 	void		*walls_img;
 	void		*exit_img;
 	void		*floor_img;
+	void		*enemy_img;
+	void		*p_direction;
 }t_map;
 
 void	check_arguments(int argc, char *argv);
@@ -84,8 +84,11 @@ void	check_path(t_map *map);
 int		end_game(void);
 int		win_game(t_map *map);
 int		detect_key(int key_code, t_map *map);
+void	movements_on_screen(t_map *map);
 void	coin_counter(t_map *map);
+void	lose_game(void);
 void	draw_map(t_map *map);
+void    move_sprit(t_map *map);
 void	ft_error(int n);
 
 #endif
