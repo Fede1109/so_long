406d1b16623f375e908bc3421aba5131a1525f5f
Federico Jose Diaz Guanoluiza <fdiaz-gu@c3r3s5.42madrid.com>
2023-10-30 16:44:32 +0100

    added enemies into interacctions and sprite movement

--- src/bonus/hooks_bonus.c
+++ src/bonus/hooks_bonus.c
@@ -6,7 +6,7 @@
 /*   By: fdiaz-gu <fdiaz-gu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/23 15:33:15 by fdiaz-gu          #+#    #+#             */
-/*   Updated: 2023/10/30 11:12:19 by fdiaz-gu         ###   ########.fr       */
+/*   Updated: 2023/10/30 16:29:24 by fdiaz-gu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,49 +14,69 @@
 
 void	go_up(t_map *map)
 {
+	if (map->map[map->player_y - 1][map->player_x] == 'X')
+		lose_game();	
 	if (map->map[map->player_y - 1][map->player_x] == 'C')
 		coin_counter(map);
 	map->map[map->player_y - 1][map->player_x] = 'P';
 	map->map[map->player_y][map->player_x] = '0';
 	map->player_y = map->player_y - 1;
 	map->movements++;
+	map->p_direction = "./sprites/up.xpm";
+	move_sprit(map);
 	ft_printf("Movimientos: %d\n", map->movements);
+	movements_on_screen(map);
 	draw_map(map);
 }
 
 void	go_down(t_map *map)
 {
+	if (map->map[map->player_y + 1][map->player_x] == 'X')
+		lose_game();
 	if (map->map[map->player_y + 1][map->player_x] == 'C')
 		coin_counter(map);
 	map->map[map->player_y + 1][map->player_x] = 'P';
 	map->map[map->player_y][map->player_x] = '0';
 	map->player_y = map->player_y + 1;
 	map->movements++;
+	map->p_direction = "./sprites/down.xpm";
+	move_sprit(map);
 	ft_printf("Movimientos: %d\n", map->movements);
+	movements_on_screen(map);
 	draw_map(map);
 }
 
 void	go_right(t_map *map)
 {
+	if (map->map[map->player_y][map->player_x + 1] == 'X')
+		lose_game();
 	if (map->map[map->player_y][map->player_x + 1] == 'C')
 		coin_counter(map);
 	map->map[map->player_y][map->player_x + 1] = 'P';
 	map->map[map->player_y][map->player_x] = '0';
 	map->player_x = map->player_x + 1;
 	map->movements++;
+	map->p_direction = "./sprites/right.xpm";
+	move_sprit(map);
 	ft_printf("Movimientos: %d\n", map->movements);
+	movements_on_screen(map);
 	draw_map(map);
 }
 
 void	go_left(t_map *map)
 {
+	if (map->map[map->player_y][map->player_x - 1] == 'X')
+		lose_game();
 	if (map->map[map->player_y][map->player_x - 1] == 'C')
 		coin_counter(map);
 	map->map[map->player_y][map->player_x - 1] = 'P';
 	map->map[map->player_y][map->player_x] = '0';
 	map->player_x = map->player_x - 1;
 	map->movements++;
+	move_sprit(map);
+	map->p_direction = "./sprites/left.xpm";
 	ft_printf("Movimientos: %d\n", map->movements);
+	movements_on_screen(map);
 	draw_map(map);
 }
 
