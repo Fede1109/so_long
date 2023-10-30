381a168dd68eb844d969b427258eb05df43bd5cb
Federico Jose Diaz Guanoluiza <fdiaz-gu@c3r3s5.42madrid.com>
2023-10-30 11:13:22 +0100

    deleted unused variables

--- include/so_long.h
+++ include/so_long.h
@@ -6,7 +6,7 @@
 /*   By: fdiaz-gu <fdiaz-gu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/16 16:42:19 by fdiaz-gu          #+#    #+#             */
-/*   Updated: 2023/10/28 20:07:18 by fdiaz-gu         ###   ########.fr       */
+/*   Updated: 2023/10/30 11:11:22 by fdiaz-gu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,9 +26,6 @@
 # define COLOR_GREEN "\033[0;32m"
 # define COLOR_BLUE "\033[0;36m"
 # define PLAYER "./sprites/down.xpm"
-# define LEFT "./sprites/down.xpm"
-# define RIGHT "./sprites/down.xpm"
-# define UP "./sprites/down.xpm"
 # define COIN "./sprites/coin.xpm"
 # define WALL "./sprites/wall.xpm"
 # define FLOOR "./sprites/floor.xpm"
@@ -46,9 +43,7 @@ typedef struct s_map
 	int			coins_copy_2;
 	int			n_players;
 	int			player_copy;
-	int			exit;
-	int			exit_copy;
-	int			exit_copy_2;
+	int			exit;		
 	int			exit_x;
 	int			exit_y;
 	char		**map;
